d68f261f5cbad3b808f521dd99bb8d2cc33fdd1e
rednaxelaMC <76746691+rednaxelaMC@users.noreply.github.com>
2021-07-27 13:14:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var perdiction_1 = "";
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera")
+
+Webcam.attach( '#camera' );
